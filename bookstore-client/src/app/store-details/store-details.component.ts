2cbf5ca309aff6607d886bcff3bb408c8c33056b
unknown <vjosiahedric1@gmail.com>
2020-08-16 18:03:54 +0800

    feature/partial commit for leaflet maps

--- bookstore-client/src/app/store-details/store-details.component.ts
+++ bookstore-client/src/app/store-details/store-details.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
 import { ActivatedRoute } from "@angular/router";
+import * as L from 'leaflet';
+import { antPath } from 'leaflet-ant-path';
 
 @Component({
   selector: 'app-list-books',
@@ -11,11 +13,13 @@ export class StoreDetailsComponent implements OnInit {
   books: Object;
   storeTags: Object;
   id: String;
+  map: L.map;
 
   constructor(private apiService: ApiService, private activetedRoute: ActivatedRoute) {}
 
   ngOnInit(): void {
     this.getBook();
+    this.generateMap();
   }
 
   async getBook(){
@@ -28,5 +32,21 @@ export class StoreDetailsComponent implements OnInit {
     (await this.apiService.getStoreTag(this.id)).subscribe(response => {
       this.storeTags = response;
     }); 
+  }
+
+  async generateMap(){
+    this.map = L.map('map').setView([14.341495, 121.0684108], 5);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'edupala.com © Angular LeafLet',
+    }).addTo(this.map);
+  
+    //not working properly, problem was ginagamit demo server
+    await L.Routing.control({
+      waypoints: [
+        L.latLng(14.343056, 121.069200),
+        L.latLng([14.388817, 121.045406])
+      ],
+      routeWhileDragging: true
+    }).addTo(this.map);
   }
 }
